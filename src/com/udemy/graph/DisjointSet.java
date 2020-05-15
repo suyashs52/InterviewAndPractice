f7e6a6a706d8c40fd110e209b321cd9d5d2f3f77
suyash kumar singh <suyashs52@gmail.com>
2020-05-15 23:45:07 +0530

    continuing disjoint set

--- src/com/udemy/graph/DisjointSet.java
+++ src/com/udemy/graph/DisjointSet.java
@@ -1,5 +1,27 @@
 package com.udemy.graph;
 
+import java.util.ArrayList;
+
 public class DisjointSet {
+	ArrayList<WeightedNode> nodelist = new ArrayList<WeightedNode>();
+
+	public static void main(String... str) {
+		ArrayList<WeightedNode> nodelist = new ArrayList<WeightedNode>();
+		for (int i = 0; i < 10; i++) {
+			nodelist.add(new WeightedNode((char) (65 + i) + ""));
+		}
+		driver(nodelist);
+
+	}
+
+	private static void driver(ArrayList<WeightedNode> nodelist) {
+		makeset(nodelist);
+
+	}
+
+	private static void makeset(ArrayList<WeightedNode> nodelist2) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
