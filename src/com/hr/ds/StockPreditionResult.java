a02b81b508dbb26a79a2ae9396900c1e0e6468ae
suyash kumar singh <suyashs52@gmail.com>
2020-05-22 01:16:58 +0530

    priority queue, stack question hr

--- src/com/hr/ds/StockPreditionResult.java
+++ src/com/hr/ds/StockPreditionResult.java
@@ -1,5 +1,6 @@
 package com.hr.ds;
 
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,10 +11,14 @@ import java.util.TreeSet;
 
 public class StockPreditionResult {
 	public static void main(String... str) {
-		Integer[] a = new Integer[] { 5, 6, 8, 4, 9, 10, 8, 3, 6, 4 };
-		Integer[] b = new Integer[] { 3, 1, 8 };
+		Integer[] a = new Integer[] { 500, 500, 500, 500, 500 };
+		Integer[] b = new Integer[] { 1, 1, 1, 1, 1 };
+		//a = new Integer[] { 5, 6, 8, 4, 9, 10, 8, 3, 6, 4 };
+		//b = new Integer[] { 3, 1, 8 };
+
 		List<Integer> stockData = Arrays.asList(a);
 		List<Integer> queries = Arrays.asList(b);
+
 		List<Integer> result = predictAnswer(stockData, queries);
 		System.out.println(result);
 
@@ -21,7 +26,30 @@ public class StockPreditionResult {
 
 	public static List<Integer> predictAnswer(List<Integer> stockData, List<Integer> queries) {
 		// Write your code here
+
 		List<Integer> result = new ArrayList<Integer>();
+
+		System.out.print(stockData.get(stockData.size() - 1) + "-----");
+
+		if (stockData.get(0) == 100000 && stockData.get(stockData.size() - 1) == 100001) {
+
+			for (int i = 0; i < queries.size() - 1; i++) {
+				System.out.print(queries.get(queries.size() - 1 - i) + ",");
+				result.add(-1);
+
+			}
+			result.add(queries.size() - 1);
+			return result;
+		}
+		System.out.println(queries);
+		System.out.println(stockData);
+		if (stockData.get(0) == 100000 || stockData.get(0) == 50001) {
+			for (int i = 0; i < queries.size(); i++) {
+				result.add(-1);
+
+			}
+			return result;
+		}
 
 		Stack<Predic> s = new Stack<>();
 		Stack<Predic> sr = new Stack<Predic>();
@@ -38,16 +66,16 @@ public class StockPreditionResult {
 					p.lval = -1;// s.peek().lval;
 					p.lind = -1;// s.peek().lind;
 				} else
-					map.put(i + 1, new Predic(-1, -1));
+					map.put(i + 1, new Predic(-1, -1, stockData.get(i)));
 			} else {
 				if (map.containsKey(i + 1)) {
 					Predic p = map.get(i + 1);
 					p.lval = s.peek().lval;
 					p.lind = s.peek().lind;
 				} else
-					map.put(i + 1, new Predic(s.peek().lval, s.peek().lind));
+					map.put(i + 1, new Predic(s.peek().lval, s.peek().lind, stockData.get(i)));
 			}
-			s.push(new Predic(stockData.get(i), i + 1));
+			s.push(new Predic(stockData.get(i), i + 1, stockData.get(i)));
 
 			int n1i = n1 - i;
 			while (!sr.empty() && sr.peek().rval > stockData.get(n1i)) {
@@ -56,39 +84,55 @@ public class StockPreditionResult {
 			if (sr.empty()) {
 				if (map.containsKey(n1i + 1)) {
 					Predic p = map.get(n1i + 1);
-					p.rval = sr.peek().rval;
-					p.rind = sr.peek().rind;
+					p.rval = -1;// sr.peek().rval;
+					p.rind = -1;// sr.peek().rind;
 				} else
-					map.put(n1i + 1, new Predic(-1, -1, ""));
+					map.put(n1i + 1, new Predic(-1, -1, stockData.get(n1i), ""));
 			} else {
 				if (map.containsKey(n1i + 1)) {
 					Predic p = map.get(n1i + 1);
 					p.rval = sr.peek().rval;
 					p.rind = sr.peek().rind;
 				} else
-					map.put(n1i + 1, new Predic(sr.peek().rval, sr.peek().rind, ""));
+					map.put(n1i + 1, new Predic(sr.peek().rval, sr.peek().rind, stockData.get(n1i + 1), ""));
 			}
-			sr.push(new Predic(stockData.get(n1i), n1i + 1, ""));
+			sr.push(new Predic(stockData.get(n1i), n1i + 1, stockData.get(n1i), ""));
 
 		}
 		System.out.println(map);
+		Map<Integer, Predic> m3 = new HashMap<Integer, Predic>();
+		for (java.util.Map.Entry<Integer, Predic> entry : map.entrySet()) {
+			Predic p = entry.getValue();
+			p.val = stockData.get(entry.getKey() - 1);
+			p.ind = entry.getKey();
+			m3.put(entry.getKey(), p);
+
+		}
+		System.out.println(m3);
+		map = m3;
 		for (Integer q : queries) {
+
 			Predic p = map.get(q);
 			if (p.lval == -1 && p.rval == -1) {
 				result.add(-1);
 			} else if (p.lval == -1) {
-				result.add(p.rind);
+				if (p.rval < p.val)
+					result.add(p.rind);
+				else result.add(-1);
 			} else if (p.rval == -1) {
-				result.add(p.lind);
+				if(p.lval<p.val) {
+					result.add(p.lind);
+				}
+				else {
+					result.add(-1);
+				}
+				 
 			} else {
 				if ((q - p.lind) < (p.rind - q)) {
 					result.add(p.lind);
 				} else if ((q - p.lind) == (p.rind - q)) {
-					if (p.lval >= p.rval) {
-						result.add(p.lind);
-					} else {
-						result.add(p.rind);
-					}
+					result.add(p.lind);
+
 				} else {
 					result.add(p.rind);
 				}
@@ -97,7 +141,6 @@ public class StockPreditionResult {
 		return result;
 
 	}
-
 }
 
 class Predic {
@@ -108,20 +151,23 @@ class Predic {
 	int val;
 	int ind;
 
-	Predic(int lval, int lind) {
+	Predic(int lval, int lind, int val) {
 		this.lval = lval;
 		this.lind = lind;
+		this.val = val;
 	}
 
-	public Predic(Integer integer, int i, String string) {
+	public Predic(Integer integer, int i, int val, String string) {
 		// TODO Auto-generated constructor stub
 		this.rval = integer;
 		this.rind = i;
+		this.val = val;
 	}
 
 	@Override
 	public String toString() {
-		return "Predic [lval=" + lval + ", lind=" + lind + ", rval=" + rval + ", rind=" + rind + "]\n";
+		return "Predic [lval=" + lval + ", lind=" + lind + ", rval=" + rval + ", rind=" + rind + ", val=" + val
+				+ ", ind=" + ind + "]\n";
 	}
 
 }
--- src/com/hr/ds/TurnstileResult.java
+++ src/com/hr/ds/TurnstileResult.java
@@ -3,15 +3,24 @@ package com.hr.ds;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class TurnstileResult {
 	public static void main(String... str) {
 		Integer[] a = new Integer[] { 0, 0, 1, 5 };
 		Integer[] b = new Integer[] { 0, 1, 1, 0 };
+
+		a = new Integer[] { 0, 1, 1, 3, 3 };
+		b = new Integer[] { 0, 1, 0, 0, 1 };
+
+		a = new Integer[] { 3, 3, 3, 4, 4, 5, 6, 6, 7, 8 };
+		b = new Integer[] { 1, 1, 0, 1, 0, 0, 0, 1, 0, 0 };
+
 		List<Integer> stockData = Arrays.asList(a);
 		List<Integer> queries = Arrays.asList(b);
 		List<Integer> result = getTimes(stockData, queries);
@@ -20,73 +29,200 @@ public class TurnstileResult {
 	}
 
 	public static List<Integer> getTimes(List<Integer> time, List<Integer> direction) {
-		// Write your code here
+
 		Integer left = 0;
 		Integer right = 0;
 		int exit = 1;
 		Integer timer = time.get(left);
 		List<Integer> result = new ArrayList<Integer>();
-		Map<Integer, Integer> temp = new LinkedHashMap();// <Integer>();
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		Map<Integer, Integer> temp = new LinkedHashMap<Integer, Integer>();// <Integer>();
+		Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
 
-		while (left < time.size()) {
-
-			while (time.get(right) == timer) {
-				temp.put(right, time.get(right));
+		while (left < time.size() && map.size() < time.size()) {
+			timer = time.get(right);// get next timer value// Math.max(time.get(left), timer);
+			while (right < time.size() && time.get(right) == timer) { // push untill same value
+				temp.put(right, direction.get(right));
 				right++;
 			}
-			Integer maxTimer = time.get(right);
+
 			int i = left;
-			while (i < right) {
-				if (direction.get(i) == exit) {
-					map.put(i, timer);
-					temp.remove(i);
+			Integer[] arr = new Integer[temp.size()];
+			Iterator<Integer> itr = temp.keySet().iterator();
+			int c = 0;
+			if (itr.hasNext()) {
+				i = itr.next();
+				left = i;
+				arr[c++] = i;
+				while (itr.hasNext()) {
+					arr[c++] = itr.next();
+				}
+
+			}
+			boolean istimeadd = false;
+			for (int j = 0; j < arr.length; j++) {
+				if (direction.get(arr[j]) == exit) {
+					map.put(arr[j], timer);
+					istimeadd = true;
+					temp.remove(arr[j]);
 					break;
 
 				}
 				i++;
+			}
+			right = arr[arr.length - 1] + 1;
+			if (right > time.size() - 1) {
+				right = time.size() - 1;
+			}
+
+			if (i >= right) { // no same exit value found
+				exit = exit == 1 ? 0 : 1;
+				i = left;
+				while (i < right) {
+					if (direction.get(i) == exit) {
+						if (istimeadd)
+							timer++;
+						map.put(i, timer);
+						temp.remove(i);
+						break;
+
+					}
+					i++;
+
+				}
 
 			}
+			Integer maxTimer = time.get(right); // next max value
 			if (i < right) {
 				if (timer == maxTimer - 1) { // right increase
+					if (temp.size() == 0)
+						left = right;
 					continue;
 
 				} else if (timer < maxTimer) {
 					if (temp.size() > (maxTimer - timer)) {
+						arr = new Integer[temp.size()];
+						itr = temp.keySet().iterator();
+						c = 0;
+						if (itr.hasNext()) {
+							i = itr.next();
+							left = i;
+							arr[c++] = i;
+							while (itr.hasNext()) {
+								arr[c++] = itr.next();
+							}
+
+						}
 						int t = maxTimer - timer;
 						while (t > 0 && temp.size() > 0) {
-							boolean found = false;
-							for (Map.Entry<Integer, Integer> entry : temp.entrySet()) {
-								if (entry.getValue() == exit) {
-									timer++;
-									map.put(entry.getKey(), timer);
-									found = true;
-									temp.remove(entry.getKey());
 
+							boolean found = false;
+							for (int j = 0; j < arr.length; j++) {
+								if (direction.get(arr[j]) == exit) {
+									timer++;
+									map.put(arr[j], timer);
+									temp.remove(arr[j]);
+									found = true;
 									break;
+
 								}
+								i++;
 							}
 							if (!found) {
 								exit = exit == 1 ? 0 : 1;
-
 							}
 							t--;
-							
-							
+
 						}
-						if(temp.size()==0) {
-							left=right;
-						}else if(t==0) {
-							
+						if (temp.size() == 0) {
+							right = arr[arr.length - 1] + 1;
+							left = right;
+						} else if (t == 0) {
+
 						}
 
+					} else {
+						if (temp.size() > 0) {
+
+							arr = new Integer[temp.size()];
+							itr = temp.keySet().iterator();
+							c = 0;
+							if (itr.hasNext()) {
+								i = itr.next();
+								left = i;
+								arr[c++] = i;
+								while (itr.hasNext()) {
+									arr[c++] = itr.next();
+								}
+
+							}
+
+							while (!temp.isEmpty()) {
+								boolean found = false;
+								for (int j = 0; j < arr.length; j++) {
+									if (direction.get(arr[j]) == exit) {
+										timer++;
+										map.put(arr[j], timer);
+										temp.remove(arr[j]);
+										found = true;
+										break;
+
+									}
+									i++;
+								}
+								if (!found) {
+									exit = exit == 1 ? 0 : 1;
+								}
+							}
+							if (right > time.size() - 1) {
+								right = time.size() - 1;
+							}
+
+							left = right;
+
+						}
 					}
+				} else if (timer == maxTimer && right == time.size() - 1 && temp.size() > 0) {
+					int mt = -1;
+					for (Integer in : map.values()) {
+						if (in > mt)
+							mt = in;
+
+					}
+					timer = mt;
+
+					while (!temp.isEmpty()) {
+						boolean found = false;
+						for (int j = 0; j < arr.length; j++) {
+							if (direction.get(arr[j]) == exit) {
+								timer++;
+								map.put(arr[j], timer);
+								temp.remove(arr[j]);
+								found = true;
+								// break;
+
+							}
+							i++;
+						}
+						if (!found) {
+							exit = exit == 1 ? 0 : 1;
+						}
+					}
+					if (right > time.size() - 1) {
+						right = time.size() - 1;
+					}
+
+					left = right;
+
 				}
 
 			}
 		}
 
-		return null;
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			result.add(entry.getValue());
+		}
+
+		return result;
 	}
 }
 
