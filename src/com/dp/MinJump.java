04174a3b5e966e92f2d60c250a2792c67c11a0d9
suyash kumar singh <suyashs52@kotak.com>
2021-01-03 21:11:44 +0530

    add min dp solv

--- src/com/dp/MinJump.java
+++ src/com/dp/MinJump.java
@@ -8,6 +8,7 @@ public class MinJump {
 		// 1->3>6->->9->5
 
 		System.out.println(recrSol(arr, 0, arr.length - 1));
+		System.out.println(dynSol(arr,arr.length));
 
 	}
 
@@ -20,8 +21,9 @@ public class MinJump {
 			return Integer.MAX_VALUE; // nothing is reachable from first (source)
 
 		int min = Integer.MAX_VALUE;
-			//2 1 3 2 , if i=1,h=3, 1+1=2 arr[2]=3, so a[h] can't reach 2nd condition is the same
-		for (int i = f+1; i <= l && i <= f + arr[f]; i++) { 
+		// 2 1 3 2 , if i=1,h=3, 1+1=2 arr[2]=3, so a[h] can't reach 2nd condition is
+		// the same
+		for (int i = f + 1; i <= l && i <= f + arr[f]; i++) {
 			int p = recrSol(arr, i, l); // calculate path first i last to l so return 0 if not max, increment path
 			if (p != Integer.MAX_VALUE && min > p + 1) {
 				min = p + 1;
@@ -30,6 +32,32 @@ public class MinJump {
 
 		return min;
 
+	}
+
+	public static int dynSol(int[] arr, int n) {
+		// 1, 3, 6, 3, 2, 3, 6, 8, 9, 5
+		// for each index calculate min distance from starting
+		// 3 can be 1, for 6 , from 1 0, from 3 it is 1
+		// for 2, from 1 -0, 3-0,6-3, 3-4 so min is 3
+
+		// 2 for loop needed
+
+		int[] jumps = new int[n];
+
+		jumps[0] = 0;
+
+		for (int i = 1; i < n; i++) {
+				jumps[i] =Integer.MAX_VALUE;
+			for (int j = 0; j < i; j++) {
+				if(jumps[j]!=Integer.MAX_VALUE && j+arr[j]>=i) {
+					jumps[i] =Math.min(jumps[i], jumps[j]+1);
+					break;
+				}
+			}
+
+		}
+
+		return jumps[n - 1];
 	}
 
 }
