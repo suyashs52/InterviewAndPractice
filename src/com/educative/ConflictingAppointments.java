766b7defc72218c48ca102d661be6f32700586ac
suyash kumar singh <suyashs52@gmail.com>
2021-06-12 23:58:23 +0530

    Update ConflictingAppointments.java

--- src/com/educative/ConflictingAppointments.java
+++ src/com/educative/ConflictingAppointments.java
@@ -4,55 +4,23 @@ import java.util.*;
 
 public class ConflictingAppointments {
 
-	public static Interval[] merge(Interval[] arr1, Interval[] arr2) {
-		List<Interval> intervalsIntersection = new ArrayList<Interval>();
-
-		int i = 0;
-		int j = 0;
-		// two array check min time interval
-		while (true) {
-			Interval o = arr1[i];
-			Interval s = arr2[j];
-
-			if (o.start <= s.start && o.end >= s.start) { // (1,3) (2,4) merged 1,(4,7) (6,8) //return the common interval
-
-				intervalsIntersection.add(new Interval(s.start, Math.min(o.end, s.end)));
-			}else if(s.start<=o.start && s.end >=o.start) {
-				intervalsIntersection.add(new Interval(o.start, Math.min(o.end, s.end)));
-				
-			}
-			//disjoint interval
-			//increment i not j to check next overlapping between next arr1,curr arr2 as arr1 has lesser time
-			if(o.end < s.end) {
-				i++; 
-			}else {
-				j++;
-			}
-			
-			if(i>= arr1.length || j>=arr2.length) {
-				break;
-			}
-
-		}
-
-		return intervalsIntersection.toArray(new Interval[intervalsIntersection.size()]);
-	}
+	  public static boolean canAttendAllAppointments(Interval[] intervals) {
+        // TODO: Write your code here
+         
+    }
 
 	public static void main(String[] args) {
-		Interval[] input1 = new Interval[] { new Interval(1, 3), new Interval(5, 6), new Interval(7, 9) };
-		Interval[] input2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
-		Interval[] result = ConflictingAppointments.merge(input1, input2);
-		System.out.print("Intervals Intersection: ");
-		for (Interval interval : result)
-			System.out.print("[" + interval.start + "," + interval.end + "] ");
-		System.out.println();
+		Interval[] intervals = { new Interval(1, 4), new Interval(2, 5), new Interval(7, 9) };
+        boolean result = ConflictingAppointments.canAttendAllAppointments(intervals);
+        System.out.println("Can attend all appointments: " + result);
 
-		input1 = new Interval[] { new Interval(1, 3), new Interval(5, 7), new Interval(9, 12) };
-		input2 = new Interval[] { new Interval(5, 10) };
-		result = ConflictingAppointments.merge(input1, input2);
-		System.out.print("Intervals Intersection: ");
-		for (Interval interval : result)
-			System.out.print("[" + interval.start + "," + interval.end + "] ");
+        Interval[] intervals1 = { new Interval(6, 7), new Interval(2, 4), new Interval(8, 12) };
+        result = ConflictingAppointments.canAttendAllAppointments(intervals1);
+        System.out.println("Can attend all appointments: " + result);
+
+        Interval[] intervals2 = { new Interval(4, 5), new Interval(2, 3), new Interval(3, 6) };
+        result = ConflictingAppointments.canAttendAllAppointments(intervals2);
+        System.out.println("Can attend all appointments: " + result);
 
 	}
 
