08491f7e3b52ea0f1f5b2014d601b8ba05e84e6c
suyash kumar singh <suyashs52@gmail.com>
2021-10-01 13:19:52 +0530

    Update TaskScheduler.java
    adding edge condition

--- src/com/educative/topkelement/TaskScheduler.java
+++ src/com/educative/topkelement/TaskScheduler.java
@@ -13,6 +13,7 @@ public class TaskScheduler {
 
 	public static int scheduleTasks(char[] tasks, int k) {
 		int s = 0;
+		if(tasks.lenght()==0) return s;
 
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 
